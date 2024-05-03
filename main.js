43eb15dcb249626f5a005a64ea0e573ffa499352
Shohjahon <shohjahon@MacBook-Air-Shohjahon.local>
2024-05-03 22:36:33 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+// const handleSubmit =(e)=>{
+// 	e.preventDefault();
+// 	let result = document.getElementById('result')
+// 	let num1 = Number(e.target[0].value)
+// 	let num2 = Number(e.target[1].value)
+// 	let amal = e.target[2].value
+// 	console.log(result);
+// 	if(amal === "+") {
+// 		result.innerText = num1 + num2
+// 	}else if (amal === "-") {
+// 		result.innerText = num1 - num2
+// 	}else if (amal === "*") {
+// 		result.innerText = num1 * num2
+// 	}else {
+// 		result.innerText = num1 / num2
+// 	}
+// }
